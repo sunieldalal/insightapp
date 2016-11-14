eb766ae11fa7596d1a59afca82e99dfb92c83d3d
Sunil Dalal <sdalal@financialengines.com>
2016-11-13 19:21:39 -0800

    initial springboot template

---
+++ src/main/java/com/slabs/insight/InsightappApplication.java
@@ -0,0 +1,12 @@
+package com.slabs.insight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightappApplication.class, args);
+	}
+}
